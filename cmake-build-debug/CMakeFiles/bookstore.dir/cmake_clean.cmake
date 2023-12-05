723226edaa55dd4189545390ed3caffbf1ce9d1e
oguri <120602715@sjtu.edu.cn>
2023-12-05 17:58:37 +0800

    design

---
+++ cmake-build-debug/CMakeFiles/bookstore.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bookstore.dir/main.cpp.o"
+  "CMakeFiles/bookstore.dir/main.cpp.o.d"
+  "bookstore"
+  "bookstore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bookstore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
